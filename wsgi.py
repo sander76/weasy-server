fb8252a08d8c250deb484596d055ab6e83e768b9
sanders76 <s.teunissen@gmail.com>
2016-11-28 12:42:53 +0100

    enabled logging1

--- wsgi.py
+++ wsgi.py
@@ -9,6 +9,7 @@ from flask.helpers import send_file
 from weasyprint import HTML
 from werkzeug.exceptions import abort
 
+import logger
 from logger.mylogger import setup_logging
 
 lgr = logging.getLogger(__name__)
@@ -20,20 +21,20 @@ def index():
     return 'ok'
 
 
-@app.before_first_request
-def setup_logging():
-    logging.addLevelName(logging.DEBUG, "\033[1;36m%s\033[1;0m" % logging.getLevelName(logging.DEBUG))
-    logging.addLevelName(logging.INFO, "\033[1;32m%s\033[1;0m" % logging.getLevelName(logging.INFO))
-    logging.addLevelName(logging.WARNING, "\033[1;33m%s\033[1;0m" % logging.getLevelName(logging.WARNING))
-    logging.addLevelName(logging.ERROR, "\033[1;31m%s\033[1;0m" % logging.getLevelName(logging.ERROR))
-
-    handler = logging.StreamHandler()
-    handler.setFormatter(logging.Formatter(
-        '%(asctime)s %(levelname)s: %(message)s '
-        '[in %(pathname)s:%(lineno)d]'
-    ))
-    app.logger.addHandler(handler)
-    app.logger.setLevel(logging.DEBUG)
+# @app.before_first_request
+# def setup_logging():
+#     logging.addLevelName(logging.DEBUG, "\033[1;36m%s\033[1;0m" % logging.getLevelName(logging.DEBUG))
+#     logging.addLevelName(logging.INFO, "\033[1;32m%s\033[1;0m" % logging.getLevelName(logging.INFO))
+#     logging.addLevelName(logging.WARNING, "\033[1;33m%s\033[1;0m" % logging.getLevelName(logging.WARNING))
+#     logging.addLevelName(logging.ERROR, "\033[1;31m%s\033[1;0m" % logging.getLevelName(logging.ERROR))
+#
+#     handler = logging.StreamHandler()
+#     handler.setFormatter(logging.Formatter(
+#         '%(asctime)s %(levelname)s: %(message)s '
+#         '[in %(pathname)s:%(lineno)d]'
+#     ))
+#     app.logger.addHandler(handler)
+#     app.logger.setLevel(logging.DEBUG)
 
 
 @app.route('/')
@@ -83,6 +84,7 @@ def get_from_url():
 
 if __name__ == '__main__':
     setup_logging("logger/log_config.json")
+
     lgr = logging.getLogger(__name__)
     lgr.debug("starting.....")
     app.run(port=5001, host="0.0.0.0")
