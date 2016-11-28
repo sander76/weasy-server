45ac181a9d7677b45256951861cb63dfb9ba9b6f
sanders76 <s.teunissen@gmail.com>
2016-11-28 14:20:12 +0100

    adding fake HTML

---
+++ fakeHTML.py
@@ -0,0 +1,3 @@
+class HTML():
+    def __init__(self,*args):
+        pass
--- wsgi.py
+++ wsgi.py
@@ -7,9 +7,10 @@ import logging
 from flask import Flask, request
 from flask.helpers import send_file
 from weasyprint import HTML
+
 from werkzeug.exceptions import abort
 
-import logger
+#from fakeHTML import HTML
 from logger.mylogger import setup_logging
 
 lgr = logging.getLogger(__name__)
@@ -19,23 +20,6 @@ app = Flask('pdf')
 @app.route('/health')
 def index():
     return 'ok'
-
-
-# @app.before_first_request
-# def setup_logging():
-#     logging.addLevelName(logging.DEBUG, "\033[1;36m%s\033[1;0m" % logging.getLevelName(logging.DEBUG))
-#     logging.addLevelName(logging.INFO, "\033[1;32m%s\033[1;0m" % logging.getLevelName(logging.INFO))
-#     logging.addLevelName(logging.WARNING, "\033[1;33m%s\033[1;0m" % logging.getLevelName(logging.WARNING))
-#     logging.addLevelName(logging.ERROR, "\033[1;31m%s\033[1;0m" % logging.getLevelName(logging.ERROR))
-#
-#     handler = logging.StreamHandler()
-#     handler.setFormatter(logging.Formatter(
-#         '%(asctime)s %(levelname)s: %(message)s '
-#         '[in %(pathname)s:%(lineno)d]'
-#     ))
-#     app.logger.addHandler(handler)
-#     app.logger.setLevel(logging.DEBUG)
-
 
 @app.route('/')
 def home():
@@ -84,7 +68,6 @@ def get_from_url():
 
 if __name__ == '__main__':
     setup_logging("logger/log_config.json")
-
     lgr = logging.getLogger(__name__)
     lgr.debug("starting.....")
     app.run(port=5001, host="0.0.0.0")
