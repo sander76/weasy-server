1e54cad587e9730c7faf2a20d4006aa8d4fa9992
sanders76 <s.teunissen@gmail.com>
2016-11-29 08:41:04 +0100

    adding env

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-cd "/home/admin-s/weasy-server/"
+cd "/home/admin-s/envs/weasy/"
+source bin/activate
 
+cd "/home/admin-s/weasy-server/"
 # gunicorn --bind 0.0.0.0:5001 wsgi:app
 python wsgi.py
