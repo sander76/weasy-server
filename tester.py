b44f314a6a3397c8f792783a9c678d938af1a7df
sanders76 <s.teunissen@gmail.com>
2016-12-01 16:41:36 +0100

    changed tester

--- tester.py
+++ tester.py
@@ -3,5 +3,6 @@ from weasyprint import HTML
 URL = "http://www.google.com/"
 OUT = "/home/admin-s/test.pdf"
 
-for lp in range(0, 200):
+for lp in range(0, 300):
+    print(str(lp))
     HTML(URL).write_pdf(OUT)
