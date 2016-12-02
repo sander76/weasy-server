1de440b6cc1535b1d846cdb19a3d8a3036f2cfe2
sanders76 <s.teunissen@gmail.com>
2016-12-02 10:55:38 +0100

    cleaning test

--- tester.py
+++ tester.py
@@ -1,8 +1,13 @@
 from weasyprint import HTML
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 URL = "http://tools.hde.nl/menc/site/guides/Pliss%C3%A9%20%26%20Duette%C2%AE%20Bottom-Up%20programming/"
 OUT = "/home/admin-s/test.pdf"
 
 for lp in range(0, 300):
-    print(str(lp))
-    HTML(URL).write_pdf(OUT)
+    try:
+        HTML(URL).write_pdf(OUT)
+    except OSError as e:
+        logging.exception("**************** ERROR AT ATTEMPT: {} *******************".format(lp))
