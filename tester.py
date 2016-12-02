9e197150f1b82b33f0a7be00b5bd588b77ac03a8
sanders76 <s.teunissen@gmail.com>
2016-12-02 10:32:36 +0100

    testing with outside world example

--- tester.py
+++ tester.py
@@ -1,6 +1,6 @@
 from weasyprint import HTML
 
-URL = "http://193.67.229.68:8000/powerview/_troubleshooting/index.html"
+URL = "http://tools.hde.nl/menc/site/guides/Pliss%C3%A9%20%26%20Duette%C2%AE%20Bottom-Up%20programming/"
 OUT = "/home/admin-s/test.pdf"
 
 for lp in range(0, 300):
