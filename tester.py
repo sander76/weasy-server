d1c4284ee105d8d2ccacc4c10b062f1dcca80f50
sanders76 <s.teunissen@gmail.com>
2016-12-02 09:55:09 +0100

    another test

--- tester.py
+++ tester.py
@@ -1,6 +1,6 @@
 from weasyprint import HTML
 
-URL = "http://www.google.com/"
+URL = "http://193.67.229.68:8000/powerview/_troubleshooting/index.html"
 OUT = "/home/admin-s/test.pdf"
 
 for lp in range(0, 300):
